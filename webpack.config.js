a5ab9f52c61a504fc1deda34854733e23de54b67
dga80 <dani.dga@gmail.com>
2024-04-12 10:47:11 +0200

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path');
+
+module.exports = {
+  entry: './script.js', // Archivo de entrada (punto de inicio)
+  output: {
+    filename: 'bundle.js', // Nombre del archivo de salida
+    path: path.resolve(__dirname, 'dist'), // Carpeta de destino
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/, // Regla para archivos HTML
+        use: ['html-loader'], // Cargador para procesar el HTML
+      },
+      // Puedes agregar más reglas aquí para otros tipos de archivos si es necesario
+    ],
+  },
+};
